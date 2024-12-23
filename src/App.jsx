66944f4a8f0021ccf8c91d9699b19457c107bf68
anhyelim <6nlimitless@gmail.com>
2024-12-23 22:46:50 +0900

    Page Router 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import { Route, Routes, Link } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Notfound from "./pages/Notfound";
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2 "/new" : 새로운 일기를 작성
 // 3. "/diary" : 일기를 상세히 조회
 function App() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
